840f4af3efa01075885c7945a8f2f18a5c055547
Lucas Kenzo <kenzolsh25@gmail.com>
2026-09-13 16:21:23 -0300

    feat: adiciona classes RecursoCatalogo e ItemRequisicao

---
+++ src/main/java/br/mackenzie/finops/dominio/entidades/ItemRequisicao.java
@@ -0,0 +1,21 @@
+package main.java.br.mackenzie.finops.dominio.entidades;
+
+public class ItemRequisicao {
+    private RecursoCatalogo recurso;
+    private Integer quantidade;
+    private Double custoSubtotal;
+
+    public ItemRequisicao(RecursoCatalogo recurso, Integer quantidade) {
+        this.recurso = recurso;
+        this.quantidade = quantidade;
+        this.custoSubtotal = calcularSubtotal();
+    }
+
+    private Double calcularSubtotal() {
+        return this.recurso.getCustoMensal() * this.quantidade;
+    }
+
+    public RecursoCatalogo getRecurso() { return recurso; }
+    public Integer getQuantidade() { return quantidade; }
+    public Double getCustoSubtotal() { return custoSubtotal; }
+}
